d0bc4e48332421dd328c9bad274859c652487cbb
shardakova svetlana <shardakova.s@medlinx.online>
2023-05-03 00:41:55 +0300

    Initial Commit

---
+++ GradientView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GradientView
+//
+//  Created by Svetlana Shardakova on 03.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
